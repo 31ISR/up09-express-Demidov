fd9e95e4fb0c9a87e57318ca28f613a4f0d4a1e4
1litengera <demidovgosa345@gmail.com>
2026-03-19 09:45:26 +0000

    i am lesbian

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const express = require("express")
 const db = require("./db")
+const jwt = require("jsonwebtoken")
+const bcr = require('bcryptjs')
+
 const app = express()
 
 app.use(express.json())
@@ -16,21 +19,44 @@ app.get("/users", (req, res) => {
 })
 
 app.post("/users", (req, res) => {
-    const { email, name } = req.body
+    const { email, name, password } = req.body
     try {
-        if (!email || !name)
+        if (!email || !name || !password)
             return res
                 .status(400)
                 .json({ error: "Не хватает данных" })
         const query = db
-            .prepare(`INSERT INTO users (name, email) VALUES (?, ?)`).run(name, email)
+            .prepare(`INSERT INTO users (name, email, password) VALUES (?, ?)`).run(name, email, password)
         const newUser = db
             .prepare("SELECT * FROM users WHERE id = ?").get(query.lastInsertRowid)
         res.status(200).json(newUser)
     } catch (error) {
         console.error(error)
-        res.status(500).json({error: "Что-то пошло не так"})
+        res.status(500).json({ error: "Что-то пошло не так" })
     }
 })
 
 app.listen(3000)
+
+app.delete("/users", (req, res) => {
+    const { id } = req.body
+    try {
+        if (!id) {
+            return res
+                .status(400)
+                .json({ error: "Не хватает данных" })
+        }
+        const query = db
+            .prepare(`DELETE FROM users WHERE id=?`).run(id)
+        res.status(200).json(query.changes)
+    }
+    catch (error) {
+        console.error(error)
+        res.status(500).json({ error: "Что-то пошло не так" })
+    }
+})
+
+app.put("/users",(req, res) => { 
+    const {email, name, password} = req.body
+   
+})
